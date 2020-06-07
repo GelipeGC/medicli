6622fa9d10d7dc9be353ddfdb8bd4075c36c1b90
Felipe Guzman <felipe-guzman.c@hotmail.com>
2020-06-07 19:33:00 +0000

    add procfile and deployment

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,38 +1,23 @@
-const path = require('path')
-const mix = require('laravel-mix')
-    // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+let mix = require('laravel-mix');
 
-//mix.config.vue.esModule = true
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
 
 mix
     .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-
-.sourceMaps()
-    //   .disableNotifications()
+    .sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
-    mix.version()
-
-    mix.extract([
-        'vue',
-        'vform',
-        'axios',
-        'vuex',
-        'jquery',
-        'popper.js',
-        'vue-i18n',
-        'vue-meta',
-        'js-cookie',
-        'bootstrap',
-        'vue-router',
-        'sweetalert2',
-        'vuex-router-sync',
-        '@fortawesome/vue-fontawesome',
-        '@fortawesome/fontawesome-svg-core'
-    ])
+    mix.version();
 }
-
 mix.webpackConfig({
     plugins: [
         // new BundleAnalyzerPlugin()
@@ -42,9 +27,6 @@ mix.webpackConfig({
         alias: {
             '~': path.join(__dirname, './resources/js')
         }
-    },
-    output: {
-        chunkFilename: 'js/[name].[chunkhash].js',
-        publicPath: mix.config.hmr ? '//localhost:8080' : '/'
     }
+
 })
